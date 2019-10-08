494f68211f053ed1f39d228b47acde187dc844f8
Ian <ianstangebye@gmail.com>
2019-10-08 09:59:55 +0200

    Trying to get back the develop branch

---
+++ src/components/game-list-item/GameList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Styles from './GameList.module.css';
+import GameItem from '../game-item/GameItem';
+import { read } from 'fs';
+
+class GameList extends React.Component{
+    state = {
+        games: []
+    }
+}
+
+componentDidMount(){
+    fetch('').then(resp=> resp.json())
+    .then(resp=>{
+        console.log(resp);
+        this.ListeningStateChangedEvent({
+           games:  [...resp.results]
+        });
+    }).catch(error=>{
+        console.log(error);
+        
+    });
+}
+
+
+render(){
+
+    let gameComponents = null;
+    if(this.state.games.length>0){
+        gameComponents = this.state.games.map(game=>{
+            return <GameItem />
+        });
+    } else {
+        gameComponents = <p>Loading games...</p>
+    }
+
+    return (
+        <React.Fragment>
+            <h1>Currently available games</h1>
+            <div>
+                {gameComponents}
+            </div>
+        </React.Fragment>
+    )
+}
