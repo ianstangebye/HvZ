13d2853a567cdc9cd60c81c1ee8f95cf88183bce
Ian <ianstangebye@gmail.com>
2019-10-08 09:32:25 +0200

    Added a game item component

---
+++ src/components/game-item/GameItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styles from './CardList.module.css';
+
+class GameItem(props){
+    const {gameItem} = props;
+
+    return (
+        <div>
+            <h4>Card.name</h4>
+            <p>Number of registered players</p>
+            <p>Relevant dates</p>
+            <p>Game Status</p>
+        </div>
+    )
+}
+
+export default GameItem;
+
