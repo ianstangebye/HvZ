7ac93c8b9c7eddac7519d6a7a8c9fc9ad55340f9
Malin <malinmaalo@gmail.com>
2019-10-16 08:30:49 +0200

    update: styling RegisterForm

--- src/components/register-form/RegisterForm.js
+++ src/components/register-form/RegisterForm.js
@@ -56,25 +56,25 @@ export default class RegisterForm extends React.Component {
                 <form>
                 <div className="FirstName">
                         <label>First Name</label>
-                        <input autoFocus type="text" name="firstname" maxLength="50" value={this.state.firstName} onChange={(e) => this.updateInputValue("firstName", e)}/>
+                        <input autoFocus type="text" name="firstname" placeholder="Your first name" maxLength="50" value={this.state.firstName} onChange={(e) => this.updateInputValue("firstName", e)}/>
                     </div>
                     <div className="LastName">
                         <label>Last Name</label>
-                        <input type="text" name="lastname" maxLength="50" value={this.state.lastName} onChange={(e) => this.updateInputValue("lastName", e)}/>
+                        <input type="text" name="lastname" placeholder="Your last name" maxLength="50" value={this.state.lastName} onChange={(e) => this.updateInputValue("lastName", e)}/>
                     </div>
                     <div className="Username">
                         <label>Username</label>
-                        <input type="text" name="username" maxLength="25" value={this.state.username} onChange={(e) => this.updateInputValue("username", e)}/>
+                        <input type="text" name="username" placeholder="Create a username" maxLength="25" value={this.state.username} onChange={(e) => this.updateInputValue("username", e)}/>
                     </div>
                     <div className="Password">
                         <label>Password</label>
-                        <input type="password" name="password" value={this.state.password} onChange={(e) => this.updateInputValue("password", e)}/>
+                        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={(e) => this.updateInputValue("password", e)}/>
                     </div>
                     
                 </form>
                 <div className={styles.Btns}>
+                <button className={styles.BtnRegister} onClick={this.handleRegisterClick}>Register</button>
                     <button className={styles.BtnSignIn} onClick={this.handleSignInClick}>Sign in</button>
-                    <button className={styles.BtnRegister} onClick={this.handleRegisterClick}>Register</button>
                 </div>
             </div>
         )
