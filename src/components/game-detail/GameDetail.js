1a78fc3249765920fd82209bef9de8c115a6e853
Malin <malinmaalo@gmail.com>
2019-10-18 08:45:26 +0200

    fix/update: squadlist

--- src/components/game-detail/GameDetail.js
+++ src/components/game-detail/GameDetail.js
@@ -155,7 +155,7 @@ class GameDetail extends React.Component {
                 <Fragment>
                     <TitleFragment game_id={game_id} />
                     <GoogleMap game_id={game_id} player={player} />
-                    <SquadListFragment game_id={game_id} player_id={player_id} />
+                    <SquadListFragment game_id={game_id} player_id={player_id} adminMode={true}/>
                     <ChatFragment adminMode={true} game_id={game_id} player_id={player_id} />
                 </Fragment>
             )
